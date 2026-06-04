80b07f238a59496051dac4eced9bff927acf185d
Thijs van Schadewijk <thijs@vanschadewijk.com>
2026-06-04 16:35:51 +0200

    fix(metadata): use static brand social card

--- src/app/twitter-image.tsx
+++
@@ -1,8 +0,0 @@
-import { OG_SIZE, OG_CONTENT_TYPE, renderOgImage } from '@/lib/og-image';
-
-export const size = OG_SIZE;
-export const contentType = OG_CONTENT_TYPE;
-
-export default async function Image() {
-  return renderOgImage();
-}
